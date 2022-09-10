8584f495ccb09bdc8a931963c763e72b0beec479
Linah-almasi <almasilinah@gmail.com>
2022-09-09 17:33:26 -0700

    zq

--- 0x01-shell_permissions/1-last_digit.c
+++ 0x01-shell_permissions/1-last_digit.c
@@ -2,28 +2,24 @@
 #include <time.h>
 #include <stdio.h>
 /**
-* main - Determines either greater than 5, is less than 6, or is 0
+* main - Entry point
 *
-* Return: Always 0 (Success)
+* Description: print the value of n status:
+*              greater than, is zero and is not less than 6.
 *
-* betty style doc for function main goes there
+* Return: Always O (Success)
 */
 int main(void)
 {
-int n, l;
+int n, digit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-l = n % 10;
-if (l > 5)
-{
-printf("Last digit of %d is %d and is greater than 5\n", n, l);
-}
-else if (l == 0)
-{
-printf("Last digit of %d is %d and is 0\n", n, l);
-}
-else
-{
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
-}
+/*my code*/
+digit = n % 10; /*gets last digit*/
+if (digit > 5)
+printf("Last digit of %i is %i and is greater than 5\n", n, digit);
+else if (digit == 0)
+printf("Last digit of %i is %i and is 0\n", n, digit);
+else if (digit < 6 && digit != 0)
+printf("Last digit of %i is %i and is less than 6 and not 0\n", n, digit);
 return (0);
